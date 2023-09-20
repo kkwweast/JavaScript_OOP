690b1b3b663bd1df7d73a7315ef80212d805bb04
Kim Weast <weastkim3@gmail.com>
2023-09-20 18:43:52 -0500

    javascript_OOP

--- app.js
+++ app.js
@@ -1,4 +1,117 @@
 console.log("Hello World!\n==========\n");
-
+//a class is a blueprint for an object
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+console.log("Exercise 1")
+
+class Person {
+    name
+    pets
+    residence
+    hobbies
+
+    constructor (name,pets, residence,hobbies) {
+        this.name = name;
+        this.pets = pets;
+        this.residence = residence;
+        this.hobbies = hobbies;
+    }
+    addHobby(hobby) {
+        //Add the hobby to the objects hobbies array
+        //
+        this.hobbies
+        this.hobbies.push(hobby);
+    }
+
+}
+removeHobby(hobbyRemove)
+    //remove the hobby from the objects hobbies array
+    //.length - 1
+    //iterate over yhe array
+    for (i = 0; i < this.hobbies.length; i++) {
+      if(this.hobbies[i] == hobbyToRemove) {
+        this.hobbies.spliced(i-- 1);
+        break;
+      }
+      this.hobbies = this.hobbies.filter(element ,index, array) =>
+      //Return true if we want to keep the item (element)
+      //Return false if we want to discard the item
+      if (element != hobbyToRemove ) {
+        //We can keep it!
+        return true;
+
+        }
+        return false;
+      };
+
+
+greeting() 
+    console.log("Hello World!");
+
+const person = new Person('Sirius Black', ["cat"] , "Grimold Place" 
+, ["sulks" , "advises" ,"inspire"] ); 
+console.log(person);
+person.removeHobby("kayaking");
+console.log(person);
+
+//exercise 2
+class coder extends person{
+ constructer(name ,pets residence, hobbies){
+  super(name, pets, residence, hobbies);
+  this.occupation = "Full Stack Web Develpoer";
+ }
+}
+class Coder = new Coder("Wade" , ["dog"] ,'162 fake Street',
+"hacking", "surfing")
+console.log(Person)
+
+//excercise 4
+class Calculator{
+  constructor(){
+    this.value = 0;
+    return this.results;
+  }
+}
+add(a ,b) {
+  if (b == undefined){
+    this.result = this.result + a;
+  }else{
+    this.result = a + b;
+  }
+  returnthis.result;
+
+}
+substract(a, b){
+  this.result = a - b;
+  return.this. result
+
+}
+multiply(a,b){
+  this.result = a * b;
+  return.this.result;
+
+}
+divide(a,b){
+  this. result = a / b;
+  return this.result;
+}
+displayResult(){
+  console.log(this.result);
+}
+
+const calc = new Calculator();
+calc.add(5,7);
+calc.displayResult();
+calc.substract(3,5);
+calc.multiply(2,7);
+calc.divide(6,5);
+calc.displayResult();
+
+calc.add(.8);
+calc.displayResult();
+calc.substract(1);
+calc.displayResult();
+calc.multiply(3);
+calc.displayResult();
+calc.divide(5);
+calc.displayResult();
